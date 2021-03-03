bc407adcb206a91c5bb1a6404e7ee19edc27ac7a
apollobyte777 <57130839+apollobyte777@users.noreply.github.com>
2021-03-04 03:02:07 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ var interval;
 var score = 0;
 var highscore = 0;
 $("#myRange").val("10");
+var question = {};
+var selections = [];
+
+
+
 
 
 var randomNumberGenerator = function(size){
@@ -15,7 +20,17 @@ var randomNumberGenerator = function(size){
 }
 
 var questionGenerator = function(){
-	var question = {};
+
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+	var selection = []
+	for(var i=0; i<checkboxes.length;i++){
+		if(checkboxes[i].checked){
+		selection.push(checkboxes[i].id);
+		}
+	}
+
+
+	question = {};
     
 
 	var myRange = document.getElementById("myRange");
@@ -30,27 +45,95 @@ var questionGenerator = function(){
 	var multiply = document.getElementById("multiply");
 	var divide = document.getElementById("divide");
 
-	if(add.checked==true){
-		question.answer = num1 + num2;
-	    question.equation = String(num1) + " + " + String(num2);
+	
 
-	} else if (subtract.checked == true){
-		question.answer = num1 - num2;
-	    question.equation = String(num1) + " - " + String(num2);
-	} else if (multiply.checked == true){
-		question.answer = num1 * num2;
-	    question.equation = String(num1) + " * " + String(num2);
+    if(selection.length == 0 || selection.length==1){
+	     if(add.checked == true){
+	      	addnumber(num1, num2);
+	      }else if (subtract.checked == true){
+			subtractnumber(num1, num2);
 
-	}else if(divide.checked == true){
-		question.answer = num1 / num2;
-	    question.equation = String(num1) + " / " + String(num2);
+		} else if (multiply.checked == true){
+			multiplynumber(num1, num2);
 
-	} else {
-		question.answer = num1 + num2;
-	    question.equation = String(num1) + " + " + String(num2);
-	}
+		}else if(divide.checked == true){
+			dividenumber(num1, num2);
+
+		} else {
+			question.answer = num1 + num2;
+		    question.equation = String(num1) + " + " + String(num2);
+		}
+    	
+    }
+
+    if (selection.length >= 2){
+    	multiplechoice(num1, num2, selection);
+    }
+
+
+
+   
+
+	
 
 	return question;
+}
+
+
+
+
+var addnumber = function(number1, number2){
+	var num1 = number1;
+	var num2 = number2;
+	question.answer = num1 + num2;
+	question.equation = String(num1) + " + " + String(num2);
+}
+
+var subtractnumber = function(number1, number2){
+	var num1 = number1;
+	var num2 = number2;
+	if(num1 > num2){
+			question.answer = num1 - num2;
+			question.equation = String(num1) + " - " + String(num2);		
+		} else {
+		   question.answer = num2 - num1;
+		   question.equation = String(num2)   + " - " + String(num1);
+	       
+	     }
+
+}
+
+var multiplynumber = function(number1, number2){
+	var num1 = number1;
+	var num2 = number2;
+	question.answer = num1 * num2;
+	question.equation = String(num1) + " * " + String(num2);
+}
+
+var dividenumber = function(number1, number2){
+	var num1 = number1;
+	var num2 = number2;
+	var num3 = num1 * num2;
+	question.answer = num3 / num1;
+	question.equation = String(num3) + " / " + String(num1);
+}
+
+var multiplechoice = function(number1, number2, selection){
+	var num1 = number1;
+	var num2 = number2;
+	var selection = selection;
+	var randomchoice = Math.floor(Math.random()*(selection.length));
+    var operation = selection[randomchoice];
+ 
+    if(operation == "add"){
+    	addnumber(num1, num2);
+    } else if (operation == "subtract"){
+    	subtractnumber(num1, num2);
+    } else if(operation == "multiply"){
+       multiplynumber(num1, num2);
+    } else if(operation == "divide"){
+    	dividenumber(num1, num2);
+    }
 }
 
 currentQuestion = questionGenerator();
